4f68f152507975b2320fa399e833894c44784939
Gajendra Babu Thokala <gajendra2k2@gmail.com>
2026-01-02 13:05:44 -0800

    Updates

--- index.bundle-app_p.js
+++ index.bundle-app_p.js
@@ -199,7 +199,7 @@ eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpa
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames not based on template
-/******/ 			if (chunkId === "app_components_Books_Books_js") return "" + chunkId + ".0b115da7a8944dbd1077.chunk.js";
+/******/ 			if (chunkId === "app_components_Books_Books_js") return "" + chunkId + ".9b5a54132a6dfd728415.chunk.js";
 /******/ 			// return url for filenames based on template
 /******/ 			return "" + chunkId + ".js";
 /******/ 		};
@@ -230,7 +230,7 @@ eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpa
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("aae69a393b4d3d9293c7")
+/******/ 		__webpack_require__.h = () => ("3f5ca639b775bd8ae7dc")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
